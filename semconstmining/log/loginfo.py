f16dc14d2f80074f18b14e3392d9457a71c6b1a3
Adrian Rebmann <adrian.rebmann@sap.com>
2023-04-05 08:32:38 +0200

    add dict and data object support; include loops in played out logs

--- semconstmining/log/loginfo.py
+++ semconstmining/log/loginfo.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from semconstmining.parsing.bert_parser import BertTagger
+from semconstmining.parsing.bert_parser.label_utils import sanitize_label
 
 
 class LogInfo:
 
     def __init__(self, bert_parser: BertTagger, labels: list = None, names: list = None, resources: list = None):
         self.labels = [] if labels is None else labels
+        self.labels = [sanitize_label(label) for label in self.labels]
         self.names = [] if names is None else names
         self.bert_parser = bert_parser
         self.activities_to_parsed = {label: self.bert_parser.parse_label(label) for label in self.labels}

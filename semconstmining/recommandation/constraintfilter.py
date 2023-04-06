ed0d86761a3faf5eed48733f34d0cbb2778558b1
Adrian Rebmann <adrian.rebmann@sap.com>
2023-04-06 10:48:49 +0200

    add storage for main components

--- semconstmining/recommandation/constraintfilter.py
+++ semconstmining/recommandation/constraintfilter.py
@@ -33,14 +33,14 @@ class ConstraintFilter:
 
     def filter_based_on_data_object(self, constraints):
         filtered_constraints = []
-        for _, row in constraints.iterrows():
+        for _, row in constraints[~constraints[self.config.DATA_OBJECT].isna()].iterrows():
             if any(item in self.filter_config.data_objects for item in row[self.config.DATA_OBJECT]):
                 filtered_constraints.append(row)
         return pd.DataFrame(filtered_constraints)
 
     def filter_based_on_dict_entries(self, constraints):
         filtered_constraints = []
-        for _, row in constraints.iterrows():
+        for _, row in constraints[~constraints[self.config.DICTIONARY].isna()].iterrows():
             if any(item in self.filter_config.dict_entries for item in row[self.config.DICTIONARY]):
                 filtered_constraints.append(row)
         return pd.DataFrame(filtered_constraints)

68ca5414adacf387214375708c52bd6a141d2f47
Adrian Rebmann <adrian.rebmann@sap.com>
2023-04-06 12:39:05 +0200

    add storage for main components

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     author='Adrian Rebmann',
     author_email='rebmann@uni-mannheim.de',
-    version="0.0.16",
+    version="0.0.17",
     description="long description",
     long_description=long_description,
     long_description_content_type="text/markdown",

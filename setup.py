10f107bd27d13c02b9cb11e0cbb5e1615fdf041e
Adrian Rebmann <adrian.rebmann@sap.com>
2023-05-23 09:03:48 +0200

    versioning

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'sentence_transformers==2.2.2',
         'mlxtend==0.21.0',
         'func-timeout==4.3.5',
-        'cvxopt==1.3.1',
         'pm4py==2.5.1',
         'gensim==4.1.2',
         "pylogics==0.1.1",
